002f80571db3df0e63e2c9417a4d58a1f2cbb3cb
onetoanother-lab <i2369096020@gmail.com>
2026-03-26 10:13:02 +0500

    d

--- server/index.js
+++ server/index.js
@@ -16,7 +16,13 @@ const PORT = 8000;
 const app = express();
 
 app.use(express.json({ limit: '10mb' }));   
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://sedap-nnap.onrender.com',
+  ],
+  credentials: true,
+}))
 
 app.use('/api', userRoutes(User));
 app.use('/api', productRoutes(Product));
