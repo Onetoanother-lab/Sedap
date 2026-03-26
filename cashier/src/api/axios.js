f4785ce6f67b7ea04b3428f51925062847be61de
onetoanother-lab <i2369096020@gmail.com>
2026-03-26 10:18:18 +0500

    d

--- cashier/src/api/axios.js
+++ cashier/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'sedap-nnap.onrender.com/api/',
+    baseURL: 'https://sedap-nnap.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
